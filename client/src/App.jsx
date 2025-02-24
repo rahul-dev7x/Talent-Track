cb5ffea624ca2d957d12a1b4da8c34637810f243
rahul-dev7x <rahulbera9932@gmail.com>
2025-02-24 15:48:06 +0530

    feat: Implemented React Router for application navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Home from "./components/user/Home"
+import SignUp from "./components/auth/SignUp"
+import Login from "./components/auth/Login"
+
+
+
+
+const appRouter=createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>
+  },
+  {
+    path:"/register",
+    element:<SignUp/>
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  }
+])
 
 
 const App = () => {
   return (
     <div>
-      App
+      <RouterProvider router={appRouter}/>
     </div>
   )
 }
